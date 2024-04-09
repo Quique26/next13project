e67645324bcffb3c873f44306f4aa0dcc3ed4a23
Quique26 <luis.nunez@cuidum.com>
2024-04-09 11:52:11 +0200

    nextproject suspense

--- app/posts/[postId]/page.jsx
+++ app/posts/[postId]/page.jsx
@@ -1,3 +1,6 @@
+import PostPages from "../page"
+import { Suspense } from "react"
+
 async function loadPost (id) {
     const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
     const data = await res.json()
@@ -11,6 +14,11 @@ async function Page({params}) {
         <div>
             <h1>{post.id} {post.title}</h1>
             <p>{post.body}</p>
+            <hr />
+            <h3>Otras Publicaciones</h3>
+            <Suspense fallback={<div>Cargando Publicaciones...</div>}>
+                <PostPages />
+            </Suspense>
         </div>
     )
 }
